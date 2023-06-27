c75e9f26efa9c7235d714c807b24bc59beeaf901
Luis Alfredo Nuñez Meneses <luis.alfredo.nu@gmail.com>
2023-06-27 16:31:16 +0200

    Create sparse matrix from the beggining

--- FSC/FSC_PY/fake_inversion.py
+++ FSC/FSC_PY/fake_inversion.py
@@ -1,4 +1,4 @@
-### Facsimile inversion 
+### Fake inversion 
 
 import numpy as np
 import itertools as it
@@ -52,7 +52,14 @@ except ImportError:
 
 clip = lambda x, l, u: l if x < l else u if x > u else x
 
-def populate_Tsm_sm(M,Lx,Ly,p_a_mu_m_xy,Tsm_sm):
+def index_six_to_two(index,M,Ly,Lx):
+    # Convert the index of an array of 6 dim to 2 dim
+    new_index_x = index[0] * Lx * Ly + index[1] * Lx + index[2]
+    new_index_y = index[3] * Lx * Ly + index[4] * Lx + index[5]
+
+    return (new_index_x,new_index_y)
+
+def build_Tsm_sm_dense(M,Lx,Ly,Lx0,Ly0,find_range,action_size,p_a_mu_m_xy):
     # Action Order
     # left, right, up, down
     
@@ -61,6 +68,11 @@ def populate_Tsm_sm(M,Lx,Ly,p_a_mu_m_xy,Tsm_sm):
     #      (m', y', x', m, y, x)
     #      pi indices : "left_act / ... "
     #      (m', a, m, y, x)
+
+    # Create the matrix
+    Tsm_sm = np.zeros( (M, Ly, Lx, M, Ly, Lx) )
+
+    # Fill the matrix
     for im_new in range(M):
         
         left = [ (im_new, iy, clip(ix-1, 0, Lx-1), im, iy, ix)   for ix in np.arange(Lx) for iy in np.arange(Ly) for im in np.arange(M)]
@@ -83,18 +95,119 @@ def populate_Tsm_sm(M,Lx,Ly,p_a_mu_m_xy,Tsm_sm):
         down_act = [ (3, im_new, im, iy, ix)   for ix in np.arange(Lx) for iy in np.arange(Ly) for im in np.arange(M)]
         for d, da in zip(down, down_act):
             Tsm_sm[d] += p_a_mu_m_xy[da]
-    # return Tsm_sm
+    
+    # Delete the rows and columns that have been set to zero depending on the distance to the initial position
+    yxs = it.product(np.arange(Ly), np.arange(Lx))
+    yx_founds = it.filterfalse(lambda x: (x[0]-Ly0)**2 + (x[1]-Lx0)**2 > find_range**2, yxs)
+    
+    for yx_found in yx_founds:
+        # all transitions starting from the source do not go anywhere
+        Tsm_sm[:,yx_found[0],yx_found[1],:,:,:] = 0
+        # all transitions ending in the source stop the episode
+        Tsm_sm[:,:,:,:,yx_found[0],yx_found[1]] = 0
+    
+    Tsm_sm_matrix = np.reshape(Tsm_sm, (M*Ly*Lx, M*Ly*Lx))
+
+    return Tsm_sm_matrix
+
+def build_Tsm_sm_sparse(M,Lx,Ly,Lx0,Ly0,find_range,action_size,p_a_mu_m_xy):
+
+    # Get the maximum number of non zeros values per row for Tsm_sm
+    count_non_zeros_in_row = np.ones(M*Ly*Lx,dtype=int) * action_size * M
+    count_non_zeros_in_row[0] -= M
+    count_non_zeros_in_row[-1] -= M
+
+    # Set the amount of non zero values to zero depending on the distance to the center
+    yxs = it.product(np.arange(Ly), np.arange(Lx))
+    yx_founds = it.filterfalse(lambda x: (x[0]-Ly0)**2 + (x[1]-Lx0)**2 > find_range**2, yxs)
+
+    for iy, ix in yx_founds:
+        for im in range(M):
+            index = np.zeros(6,dtype=int)
+            index[0] = im
+            index[1] = iy 
+            index[2] = ix
+            index_mat = index_six_to_two(index,M,Ly,Lx)
+            count_non_zeros_in_row[index_mat[0]] = 0
+
+    # Create the sparse matrix
+    Tsm_sm_sp = sparse.lil_matrix((M*Ly*Lx, M*Ly*Lx), dtype=np.double)
+
+    # Fill the sparse matrix
+    for im_new in range(M):
+        
+        left = [ (im_new, iy, clip(ix-1, 0, Lx-1), im, iy, ix)   for ix in np.arange(Lx) for iy in np.arange(Ly) for im in np.arange(M)]
+        left_act = [ (0, im_new, im, iy, ix)   for ix in np.arange(Lx) for iy in np.arange(Ly) for im in np.arange(M)]
+        
+        for t, a in zip(left, left_act):
+            indexes = index_six_to_two(t,M,Ly,Lx)
+            if Tsm_sm_sp[indexes] == None :
+                Tsm_sm_sp[indexes] =  p_a_mu_m_xy[a]
+            else:
+                Tsm_sm_sp[indexes] +=  p_a_mu_m_xy[a]
+                
+        right = [ (im_new, iy, clip(ix+1, 0, Lx-1), im, iy, ix)   for ix in np.arange(Lx) for iy in np.arange(Ly) for im in np.arange(M)]
+        right_act = [ (1, im_new, im, iy, ix)   for ix in np.arange(Lx) for iy in np.arange(Ly) for im in np.arange(M)]
+
+        for t, a in zip(right, right_act):
+            indexes = index_six_to_two(t,M,Ly,Lx)
+            if Tsm_sm_sp[indexes] == None :
+                Tsm_sm_sp[indexes] =  p_a_mu_m_xy[a]
+            else:
+                Tsm_sm_sp[indexes] +=  p_a_mu_m_xy[a]
+
+        up = [ (im_new, clip(iy+1, 0, Ly-1), ix,  im, iy, ix)   for ix in np.arange(Lx) for iy in np.arange(Ly) for im in np.arange(M)]
+        up_act = [ (2, im_new, im, iy, ix)   for ix in np.arange(Lx) for iy in np.arange(Ly) for im in np.arange(M)]
+
+        for t, a in zip(up, up_act):
+            indexes = index_six_to_two(t,M,Ly,Lx)
+            if Tsm_sm_sp[indexes] == None :
+                Tsm_sm_sp[indexes] =  p_a_mu_m_xy[a]
+            else:
+                Tsm_sm_sp[indexes] +=  p_a_mu_m_xy[a]
+        
+        down = [ (im_new, clip(iy-1, 0, Ly-1), ix, im, iy, ix)   for ix in np.arange(Lx) for iy in np.arange(Ly) for im in np.arange(M)]
+        down_act = [ (3, im_new, im, iy, ix)   for ix in np.arange(Lx) for iy in np.arange(Ly) for im in np.arange(M)]
+
+        for t, a in zip(down, down_act):
+            indexes = index_six_to_two(t,M,Ly,Lx)
+            if Tsm_sm_sp[indexes] == None :
+                Tsm_sm_sp[indexes] =  p_a_mu_m_xy[a]
+            else:
+                Tsm_sm_sp[indexes] +=  p_a_mu_m_xy[a]
+
+    # Delete the rows and columns that have been set to zero depending on the distance to the initial position
+    yxs = it.product(np.arange(Ly), np.arange(Lx))
+    yx_founds = it.filterfalse(lambda x: (x[0]-Ly0)**2 + (x[1]-Lx0)**2 > find_range**2, yxs)
+
+    for yx_found in yx_founds:
+        for im in range(M):
+            # rows to set zero
+            indexes = np.zeros(6,dtype=int)
+            indexes[0] = indexes[3] = im
+            indexes[1] = indexes[4] = yx_found[0]
+            indexes[2] = indexes[5] = yx_found[1]
+            indexes = index_six_to_two(indexes,M,Ly,Lx)
+            # rows to set zero
+            Tsm_sm_sp[indexes[0],:] = 0
+            # columns to set zero
+            Tsm_sm_sp[:,indexes[1]] = 0
+
+    return Tsm_sm_sp
 
 def solution_test(eta,T,rho,gamma):
-    sol = (np.eye(T.shape[0]) - gamma * T) @ eta
+    if sparse.issparse(T):
+        identity = sparse.eye(T.shape[0])
+    else:
+        identity = np.eye(T.shape[0])
+    sol = (identity - gamma * T) @ eta
     # plt.plot(sol-rho)
     # plt.show()
     return np.allclose(sol,rho)
-    
 
 ## Functions to the $\eta$ with different libraries
 
-def eta_petsc(Tsm_sm_matrix,gamma,M,Lx,Ly,rho0,verbose=False,device='gpu'):
+def eta_petsc(Tsm_sm_matrix,gamma,M,Lx,Ly,rho0,ks_type,ps_type,verbose=False,device='gpu'):
     """
     Solve linear system for eta using PETSc
     """
@@ -166,25 +279,24 @@ def eta_petsc(Tsm_sm_matrix,gamma,M,Lx,Ly,rho0,verbose=False,device='gpu'):
     # print('Matrix A assembled', A.size)
     # print(A.getInfo())
     # print("Options set", A.getOptionsPrefix())
-    
-    # Start the solver
-    ksp = PETSc.KSP().create(comm=A.getComm())
-    ksp.getPC().setType(PETSc.PC.Type.CHOLESKY)
-    ksp.setTolerances(rtol=1e-10)
-
-    ksp.setOperators(A)
-    # ksp.setType(PETSc.KSP.Type.CG)
-    # ksp.setConvergenceHistory()
-    ksp.setFromOptions()
-    # ksp.setInitialGuessNonzero(True)
 
     x, b =  A.createVecs()
     b.setValues(np.arange(rho0.shape[0],dtype=np.int32),rho0)
 
     b.assemble()
     x.assemble()
+    
+    # Start the solver
+    ksp = PETSc.KSP().create(comm=A.getComm())
+    ksp.setOperators(A)
+    ksp.setType(ks_type)
 
-    # print("type vector x:",x.getType(), x.getSizes(),"b:",b.getType(), b.getSizes())
+    ksp.getPC().setType(ps_type)
+    ksp.setTolerances(rtol=1e-10)
+
+    # ksp.setConvergenceHistory()
+    ksp.setFromOptions()
+    # ksp.setInitialGuessNonzero(True)
 
     ksp.setUp()
     ksp.solve(b, x)
@@ -202,7 +314,6 @@ def eta_petsc(Tsm_sm_matrix,gamma,M,Lx,Ly,rho0,verbose=False,device='gpu'):
     x.destroy()
 
     return eta
-
 
 def eta_numpy(Tsm_sm_matrix,gamma,M,Lx,Ly,rho0,verbose=False,device='cpu'):
     """
@@ -368,8 +479,14 @@ def solve_eta(pi, PObs_lim, gamma, rho0, Lx, Ly, Lx0, Ly0, find_range,func_eta=e
     # T [ s'm'  sm] = sum_a, mu p(s'm' | sm a mu) p(a mu | sm)
     #               = sum_a, mu p(s'm' | sm a mu) sum_y f(y | s) pi(a mu | y m)
     
+    need_sparse = False
+    if func_eta == eta_scipy_sparce_solve or func_eta == eta_petsc:
+        need_sparse = True
     # Tsm_sm has size ~ 10^5 x 10^5 or more
-    Tsm_sm = np.zeros( (M, Ly, Lx, M, Ly, Lx) )
+    if need_sparse:
+        Tsm_sm_matrix = build_Tsm_sm_sparse(M,Lx,Ly,Lx0,Ly0,find_range,A//M,p_a_mu_m_xy)
+    else:
+        Tsm_sm_matrix = build_Tsm_sm_dense(M,Lx,Ly,Lx0,Ly0,find_range,A//M,p_a_mu_m_xy)
 
     if verbose:
         print("-"*50)
@@ -378,37 +495,43 @@ def solve_eta(pi, PObs_lim, gamma, rho0, Lx, Ly, Lx0, Ly0, find_range,func_eta=e
         print("new_eta shape:",new_eta.shape)
         print("PY shape:",PY.shape, "PAMU shape:",PAMU.shape)
         print("p_a_mu_m_xy shape:",p_a_mu_m_xy.shape)
-        print("Tsm_sm shape:",Tsm_sm.shape,"size:",Tsm_sm.size)
-    
-    #  Populated of Tsm_sm
-    populate_Tsm_sm(M,Lx,Ly,p_a_mu_m_xy,Tsm_sm)
-    
-    yxs = it.product(np.arange(Ly), np.arange(Lx))
-    yx_founds = it.filterfalse(lambda x: (x[0]-Ly0)**2 + (x[1]-Lx0)**2 > find_range**2, yxs)
-    
-    All = slice(None)
-    for yx_found in yx_founds:
-        # print("yx_found:",yx_found)
-        # all transitions starting from the source do not go anywhere
-        ls = (All, yx_found[0], yx_found[1], All, All, All)
-        Tsm_sm[ls] = 0
-        # all transitions ending in the source stop the episode
-        ls = (All, All, All, All, yx_found[0], yx_found[1])
-        Tsm_sm[ls] = 0
-    
-    Tsm_sm_matrix = np.reshape(Tsm_sm, (M*Ly*Lx, M*Ly*Lx))
-    
-    if verbose:
-        print("number of non-zeros in Tsm_sm_matrix:",np.count_nonzero(Tsm_sm_matrix != 0))
-        print("number of     zeros in Tsm_sm_matrix:",np.count_nonzero(Tsm_sm_matrix == 0))
-        print("             Full matrix memory size:", Tsm_sm_matrix.nbytes/1e6, " MB")
-        Tsm_sm_matrix_sparce = sparse.csr_matrix(Tsm_sm_matrix)
-        print("           Sparse matrix memory size:", Tsm_sm_matrix_sparce.data.nbytes/1e6, " MB")
-        Tsm_sm_matrix_sparce = None
-        # plt.imshow(Tsm_sm_matrix)
-        # plt.show()
         
-    new_eta = func_eta(Tsm_sm_matrix,gamma,M,Lx,Ly,rho0,device=device)
+        if need_sparse:
+            print("               Type of Tsm_sm_matrix:",type(Tsm_sm_matrix))
+            print("number of non-zeros in Tsm_sm_matrix:",Tsm_sm_matrix.nnz)
+            print("number of     zeros in Tsm_sm_matrix:",M*Lx*Ly*M*Lx*Ly - Tsm_sm_matrix.nnz)
+            print("               opacity of the matrix: {:7.4f}".format(Tsm_sm_matrix.nnz/(M*Lx*Ly*M*Lx*Ly) * 100.0), "%")
+            print("           Sparse matrix memory size:", Tsm_sm_matrix.data.nbytes/1e6, " MB")
+            # plt.imshow(Tsm_sm_matrix.toarray())
+            # plt.show()
+
+        else :
+            print("               Type of Tsm_sm_matrix:",type(Tsm_sm_matrix))
+            print("number of non-zeros in Tsm_sm_matrix:",np.count_nonzero(Tsm_sm_matrix != 0))
+            print("number of     zeros in Tsm_sm_matrix:",np.count_nonzero(Tsm_sm_matrix == 0))
+            print("               opacity of the matrix: {:7.4f}".format(np.count_nonzero(Tsm_sm_matrix != 0)/Tsm_sm_matrix.size * 100.0), "%") 
+            print("             Full matrix memory size:", Tsm_sm_matrix.nbytes/1e6, " MB")
+            # plt.imshow(Tsm_sm_matrix)
+            # plt.show()
+
+    if func_eta == eta_petsc:
+        list_ksp_type = ['cg','gmres','bcgs','bcgsl','bcgsr','bicg','bicgsta','tfqmr','cr','minres','symmlq','lgmres']
+        list_pc_type = ['none','jacobi','sor','lu','cholesky','bjacobi','mg','eisenstat','ilu','icc','asm','gasm','ksp','preonly']
+
+        # for ksp_type in list_ksp_type:
+        #     for pc_type in list_pc_type:
+        #         print(ksp_type,pc_type)
+        #         try :
+        #             %timeit func_eta(Tsm_sm_matrix,gamma,M,Lx,Ly,rho0,ksp_type,pc_type)
+        #         except :
+        #             print("Incompatible arrangement")
+        # new_eta = func_eta(Tsm_sm_matrix,gamma,M,Lx,Ly,rho0,'bcgsl','sor')
+        new_eta = func_eta(Tsm_sm_matrix,gamma,M,Lx,Ly,rho0,'bcgs','sor',device=device)
+    else :
+        new_eta = func_eta(Tsm_sm_matrix,gamma,M,Lx,Ly,rho0,device=device)
+    # new_eta = func_eta(Tsm_sm_matrix,gamma,M,Lx,Ly,rho0)
+            
+    # new_eta = func_eta(Tsm_sm_matrix,gamma,M,Lx,Ly,rho0,device=device)
     
     return new_eta, Tsm_sm_matrix
 
@@ -418,7 +541,7 @@ if __name__ == "__main__":
 
     dim_factor = 1 if len(sys.argv) < 2 else int(sys.argv[1])
 
-    M = 2 # O(10) 
+    M = 5 # O(10) 
     O = 2 # 2/3
     Lx = 11 * dim_factor # O(100)-O(1000)
     Ly = 15 * dim_factor # O(100)-O(1000)        
@@ -426,7 +549,7 @@ if __name__ == "__main__":
 
     print("-"*50)
     print("System parameters:")
-    print("M:",M,"   O:",O,"   Lx:",Lx,"   Ly:",Ly,"   a_size:",a_size,"   dim_factor:",dim_factor)
+    print("M:",M,"   O:",O,"   Lx:",Lx,"   Ly:",Ly,"   # actions:",a_size,"   dim_factor:",dim_factor)
 
     # np.random.seed(33+33)
 
@@ -454,9 +577,9 @@ if __name__ == "__main__":
     # solvers = [ eta_numpy,eta_numpy_sol,eta_torch,eta_torch_sol, eta_torch, eta_torch_sol,eta_scipy_sparce_solve, eta_petsc,eta_petsc]
     # name_solvers =    ["Numpy", "Numpy Sol", "Torch CPU", "Torch SolCPU", "Torch", "Torch Sol", "Scipy", "Petsc GPU", "Petsc CPU"]
     # device_selector = [  'cpu',       'cpu',       'cpu',          'cpu',   'gpu',       'gpu',   'cpu',       'gpu',       'cpu']
-    solvers = [eta_scipy_sparce_solve, eta_petsc,eta_petsc]
-    name_solvers =    [ "Scipy", "Petsc GPU", "Petsc CPU"]
-    device_selector = [   'cpu',       'gpu',       'cpu']
+    solvers = [eta_scipy_sparce_solve,eta_petsc]
+    name_solvers =    ["Scipy", "Petsc CPU"]
+    device_selector = [  'cpu',       'cpu']
 
     # print("-"*50)
     # print("Details of the solvers:")
@@ -468,7 +591,7 @@ if __name__ == "__main__":
 
     print("-"*50)
     print("Benchmarking of the solvers")
-    runtimes = 3
+    runtimes = 2
     print("Number of runs:",runtimes)
 
     for solver, name_solver,device_sel in zip(solvers, name_solvers,device_selector):
